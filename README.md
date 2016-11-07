682c7532b13591daf49baf42fb9680e528d035f6
iBogdanov <ilya.bogdanov14@gmail.com>
2016-11-07 23:42:29 +0300

    Добавлен скрипт, производящий поиск дубликатов файлов в заданной директории

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # 11_duplicates
+
+## Описание
+
+Скрипт принимает на вход папку, просматривает все файлы в
+ней и всех подпапках и сообщает, если находит дубликаты (файлы
+с одинаковым именем и размером).
+
+## Использование
+
+Скрипт принимает на вход директорию. Задать ее можно двумя способами:
+
+* явно указать при запуске скрипта: 
+```sh
+$ python3.5 ./duplicates.py ./SomeDir
+```
+
+* запустить скрипт на выполнение командой:
+```sh
+$ python3.5 ./duplicates.py
+```
+В этом случае потребуется ввести директорию отдельно:
+```sh
+$ Enter directory > ./SomeDir
+```
+При попытке передать скрипту более двух параметров будет выведено сообщение:
+```sh
+Error: too many parameters are transferred
+```
+
+## Пример
+
+Ниже приведен пример использования скрипта и его вывод:
+```sh
+$ python3.5 ./duplicates.py ./Dir
+File File3.txt has duplicates in the following directories:
+./Dir/Dir3/File3.txt
+./Dir/Dir3/Dir32/File3.txt
+File File1.txt has duplicates in the following directories:
+./Dir/Dir1/File1.txt
+./Dir/Dir2/File1.txt
+./Dir/Dir3/File1.txt
+File File2.txt has duplicates in the following directories:
+./Dir/File2.txt
+./Dir/Dir2/File2.txt
+```
--- duplicates.py
+++ duplicates.py
@@ -1,8 +1,63 @@
+import sys
+import os
 
 
-def are_files_duplicates(file_path1, file_path_2):
-    pass
+def get_files_data(input_directory):
+    data = {}
+    for current_dir, folders, files in os.walk(input_directory):
+        for file in files:
+            file_attributes = {
+                'path': current_dir,
+                'size': os.path.getsize(current_dir + '/' + file)
+            }
+            if file in data:
+                data[file].append(file_attributes)
+            else:
+                data[file] = [file_attributes]
+    return data
+
+
+def get_files_duplicates(files_data):
+    files_duplicates = {}
+    for file_name in files_data:
+        file_name_duplicates_count = len(files_data[file_name])
+
+        if file_name_duplicates_count > 1:
+
+            file_duplicates_indexes = []
+            for i in range(0, file_name_duplicates_count):
+                size = files_data[file_name][i]['size']
+                for j in range(i + 1, file_name_duplicates_count):
+                    if size == files_data[file_name][j]['size']:
+                        file_duplicates_indexes.append(i)
+                        file_duplicates_indexes.append(j)
+            file_duplicates_indexes = set(file_duplicates_indexes)
+
+            if file_duplicates_indexes:
+                files_duplicates[file_name] = [files_data[file_name][ind] for ind in file_duplicates_indexes]
+
+    return files_duplicates
+
+
+def print_files_duplicates(files_duplicates):
+    for file_name in files_duplicates:
+        print('File {0} has duplicates in the following directories:'.format(file_name))
+        for duplicate in files_duplicates[file_name]:
+            print('{0} (Size: {1})'.format(
+                duplicate['path'] + '/' + file_name,
+                duplicate['size']
+            ))
 
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) == 1:
+        input_directory = input('Enter directory > ')
+    else:
+        if len(sys.argv) > 2:
+            print("Error: too many parameters are transferred")
+            sys.exit(1)
+        input_directory = sys.argv[1]
+
+    files_data = get_files_data(input_directory)
+    files_duplicates = get_files_duplicates(files_data)
+    print_files_duplicates(files_duplicates)
